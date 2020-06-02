cffac8f1402bf63b6a3b55308dcef44c8d87a1dd
Takeshi Aida <aida@shizentai-factory.com>
2020-06-03 03:27:17 +0900

    ボタンで高さ変更

--- src/App.js
+++ src/App.js
@@ -27,38 +27,40 @@ const data = [ { geopos: [ MAP_CENTER[ 0 ], MAP_CENTER[ 1 ] ], value: 100, desc:
 
 export default class App extends React.Component
 {
+  createLayer = ( count ) => new InfectorsLayer({
+    id: `3dgram${count}`,
+    data,
+    coverage: coverage,
+    getColorValue: d => d[ 0 ].value,
+    getElevationValue: this.getElevationValue,
+    elevationScale: 10,
+    elevationDomain: [0, MAX_INFECTORS], // 棒の高さについて、この幅で入力値を正規化する デフォルトでは各マスに入る行の密度(points.length)となる
+    elevationRange: [0, 1000],           // 入力値をelevationDomainで正規化したあと、この幅で高さを決める
+    colorDomain: [0, MAX_INFECTORS],     // 棒の色について、この幅で入力値を正規化する
+    colorRange: colorRange,
+    extruded: true,
+    getPosition: d => d.geopos,
+    opacity: 1.0,
+    pickable: true,
+    radius: 500,
+    upperPercentile: upperPercentile,
+    onHover: info => this.setState({
+      hoveredObject: info.object,
+      pointerX: info.x,
+      pointerY: info.y
+    })
+  });
+
   state = {
-    viewport: {
+    viewState: {
       latitude: MAP_CENTER[ 1 ],
       longitude: MAP_CENTER[ 0 ],
       zoom: MAP_ZOOM,
       bearing: 0,
       pitch: MAP_PITCH
     },
-    interactionState: {},
-    maplayer: new InfectorsLayer({
-      id: "heatmap",
-      data,
-      coverage: coverage,
-      getColorValue: d => d[ 0 ].value,
-      getElevationValue: this.getElevationValue,
-      elevationScale: 10,
-      elevationDomain: [0, MAX_INFECTORS], // 棒の高さについて、この幅で入力値を正規化する デフォルトでは各マスに入る行の密度(points.length)となる
-      elevationRange: [0, 1000],           // 入力値をelevationDomainで正規化したあと、この幅で高さを決める
-      colorDomain: [0, MAX_INFECTORS],     // 棒の色について、この幅で入力値を正規化する
-      colorRange: colorRange,
-      extruded: true,
-      getPosition: d => d.geopos,
-      opacity: 1.0,
-      pickable: true,
-      radius: 500,
-      upperPercentile: upperPercentile,
-      onHover: info => this.setState({
-        hoveredObject: info.object,
-        pointerX: info.x,
-        pointerY: info.y
-      })
-    })
+    layercount: 0,
+    maplayer: this.createLayer( 0 )
   };
 
   getElevationValue( d )
@@ -77,15 +79,32 @@ export default class App extends React.Component
     );
   }
 
-  render() {
-    const {viewport, interactionState, maplayer} = this.state;
+  redrawLayer()
+  {
+    this.setState(
+      (state, props) => { return { layercount: this.state.layercount ^ 1 } },
+      () => this.setState( { maplayer: this.createLayer( this.state.layercount ) } )
+    );
+  }
 
+  _onViewStateChange = ({viewState}) => {
+    this.setState( {viewState} );
+  };
+
+  onDebug01 = () =>
+  {
+    data[ 0 ].value *= 0.5;
+    this.redrawLayer();
+  }
+
+  render() {
     return (
       <DeckGL
-        initialViewState={ viewport }
+        viewState={ this.state.viewState }
+        onViewStateChange={this._onViewStateChange}
         controller={true}
         ContextProvider={MapContext.Provider}
-        layers={[ maplayer ]}
+        layers={[ this.state.maplayer ]}
       >
         <MapGL
           mapStyle={MAP_STYLE}
@@ -102,12 +121,13 @@ export default class App extends React.Component
             <input id="slider_date" type="range" min="0" max="10" step="1"/>
           </div>
           <div className="map-overlay-inner">
+            <div>person</div>
             <div id="legend" className="legend">
               <div className="bar"></div>
             </div>
             <div style={{float: "left"}}>0</div>
             <div style={{float: "right"}}>{MAX_INFECTORS}</div>
-            <div>person</div>
+            <div><br/></div>
           </div>
           <div className="map-overlay-inner">
             <fieldset>
@@ -121,10 +141,10 @@ export default class App extends React.Component
               <label>Choose a color</label>
               <div id="swatches">
                 <div className="blue">
-                  <button id="blue_button" onClick={() => { console.log("BLUE CLICKED"); }} ></button>
+                  <button id="blue_button" onClick={this.onDebug01} />
                 </div>
                 <div className="green">
-                  <button id="green_button"></button>
+                  <button id="green_button" />
                 </div>
               </div>
             </fieldset>
