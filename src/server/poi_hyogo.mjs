24c697ac04c7560203e5f5f331f982d02810c17a
Yoshiyuki Majima <63690396+uma3san@users.noreply.github.com>
2020-09-11 12:53:15 +0900

    兵庫県追加その３

---
+++ src/server/poi_hyogo.mjs
@@ -0,0 +1,55 @@
+import xlsx from 'xlsx';
+import { sanitize_poi_name } from "./util.mjs";
+import BasePoi from "./base_poi.mjs";
+const config = global.covid19map.config;
+
+const ALTER_CITY_NAMES = 
+[
+  ['伊丹健康福祉事務所管内',  '伊丹市'],
+  ['龍野健康福祉事務所管内',  'たつの市'],
+  ['加東健康福祉事務所管内',  '加東市'],
+  ['加古川健康福祉事務所管内','加古川市'],
+  ['加古川健康事務所管内',    '加古川市'],
+  ['宝塚健康福祉事務所管内',  '宝塚市'],
+  ['洲本健康福祉事務所管内',  '洲本市'],
+  ['豊岡健康福祉事務所管内',  '豊岡市'],
+  ['赤穂健康福祉事務所管内',  '赤穂市'],
+  ['中播磨健康福祉事務所管内','姫路市'],
+  ['朝来健康福祉事務所管内',  '朝来市'],
+  ['丹波健康福祉事務所管内',  '丹波市'],
+  ['芦屋健康福祉事務所管内',  '芦屋市'],
+
+];
+async function parse_xlsx( cr )
+{
+  const book = xlsx.read( cr.data, { cellDates: true } );
+  const worksheet = book.Sheets[ book.SheetNames[ 0 ] ];
+  const range = worksheet['!ref'];
+  const rows = parseInt( range.match( /^.+:[A-z]+(\d+)$/ )[ 1 ] );
+  const csv = [];
+  for ( let row = 8; row < rows; row++ )
+  {
+    const cellDate = worksheet[ `C${row}` ];
+    const cellCity = worksheet[ `G${row}` ];
+    if ( cellDate?.t !== 'd' || cellCity?.t !== 's' )
+      break;
+    csv.push( [ cellDate.v, sanitize_poi_name( cellCity.v ) ] );
+  }
+  return csv;
+}
+export default class PoiHyogo extends BasePoi
+{
+  static async load()
+  {
+    return BasePoi.process_csv( {
+      pref_name: '兵庫県',
+      alter_citys: ALTER_CITY_NAMES,
+      csv_uri: config.HYOGO_XLS.DATA_URI,
+      cb_parse_csv: cr => parse_xlsx( cr ),
+      row_begin: 0,
+      min_columns: 2,
+      col_date: 0,
+      col_city: 1
+    } );
+  }
+}
